0902968c6c0ad8f3bc9b97cd67aecfc9ccfa917a
Rick Nie <nxp1968@gmail.com>
2026-07-31 15:57:00 -0700

    some nodes completed

---
+++ lib/src/main/java/usace/hec/expressions/time/DayOfMonthNode.java
@@ -0,0 +1,45 @@
+package usace.hec.expressions.time;
+
+import usace.hec.expressions.DateTimeExpressionNode;
+import usace.hec.expressions.DisplayNode;
+import usace.hec.expressions.ExpressionNode;
+import usace.hec.expressions.ExpressionOperator;
+import usace.hec.expressions.ExpressionType;
+import usace.hec.expressions.IntegerExpressionNode;
+import usace.hec.expressions.UnaryExpressionNode;
+
+import java.io.Serial;
+
+
+public class DayOfMonthNode implements UnaryExpressionNode, IntegerExpressionNode {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private DateTimeExpressionNode child;
+    public DayOfMonthNode(DateTimeExpressionNode child) {
+        this.child = child;
+    }
+    @Override
+    public int evaluate() {
+        return child.evaluate().getDayOfMonth();
+    }
+    @Override
+    public ExpressionOperator Operator() {
+        return StaticOperator();
+    }
+    public static ExpressionOperator StaticOperator(){
+        return ExpressionOperator.DOM;
+    }
+
+    @Override
+    public String ExcelSyntax() {
+        return PreFixSyntax(); //for excel syntax in this case prefix is the correct syntax
+    }
+    @Override
+    public ExpressionType resultType() {
+        return ExpressionType.INTEGER;
+    }
+    @Override
+    public ExpressionNode child() {
+        return this.child;
+    }
+}
